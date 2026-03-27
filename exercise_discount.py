ed3df7713f806e8d16ba83f489ada042909ec650
Theo-Ve <tvernet@mail.austral.edu.ar>
2026-03-27 10:40:01 -0300

    Add files via upload

--- exercise_discount.py
+++ exercise_discount.py
@@ -53,7 +53,7 @@ def discount():
         print(f"Monto del descuento: {desc2 * subtotal / 100}")
         desc_dsp = desc2
     else:
-        print(f"Monto del descuento: {desc3 * subtotal / 100}")
+        print(f"Monto del descuento: {subtotal}")
         desc_dsp = desc3
 
     descuento = subtotal * desc_dsp / 100 # Calcula el descuento en dinero
